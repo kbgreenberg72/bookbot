8cb17033ee93a18e9356f2313f49bce63b94921c
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-03-28 16:50:19 -0400

    Moved the read code from Main.py into another function that is called by main.py. This allows for better troubleshooting within the code.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-def count_words(text):
+def get_count_words(text):
     words = text.split()
-    print(f"{words}")
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as file:
+        return file.read()
 
 def main():
-    with open("books/frankenstein.txt") as file:
-        text = file.read()
-        count_words(text)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_count_words(text)
+    print(f"{num_words} words found in the document")
         
         
 if __name__ == "__main__":
