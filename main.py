fd163281fdd8dc5712be532ec29adcb57fdc2de6
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-03-28 17:15:25 -0400

    Added a sort function for the dictionary of letter_count. I also finished the print_report function.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text: str = get_book_text(book_path)
     num_words: int = get_count_words(text)
     letter_count: dict = get_count_letters(text)
+    
     print_report(num_words, letter_count, book_path)
 
 def get_count_words(text):
@@ -17,16 +18,24 @@ def get_count_letters(text):
     lowercase_text = text.lower()
     letter_count = {}
     for char in lowercase_text:
-        if char in letter_count:
-            letter_count[char] += 1
-        else:
-            letter_count[char] = 1
+        if char.isalpha():
+            if char in letter_count:
+                letter_count[char] += 1
+            else:
+                letter_count[char] = 1
     return letter_count
+
+def sort_letter_count(letter_count):
+    # return {k: v for k, v in sorted(letter_count.items(), key=lambda item: item[1], reverse=True)}
+    return dict(sorted(letter_count.items())) 
 
 def print_report(num_words, letter_count, path):
     print(f"--- Begin report of {path} ---")
     print(f"{num_words} words found in the document\n")
-    
+    sorted_count = sort_letter_count(letter_count)
+    for letter in sorted_count:
+        print(f"The '{letter}' character was found {sorted_count[letter]} times")
+    print(f"--- End report ---")
     
         
         
