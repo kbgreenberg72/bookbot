41fa93e51906260db436e524811e1377510e5240
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-03-28 16:45:35 -0400

    Added function count_words that checks how many words are in the text.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def count_words(text):
+    words = text.split()
+    print(f"{words}")
+
 def main():
-    with open("books/frankenstein.txt", "r") as file:
+    with open("books/frankenstein.txt") as file:
         text = file.read()
+        count_words(text)
         
         
 if __name__ == "__main__":
