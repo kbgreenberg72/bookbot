013038f1c2f74f0057e742923902bc8b89038a81
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-03-28 16:57:26 -0400

    Added a function for counting letters and character symbols and shows the user how often they occur.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_count_words(text)
+    print(f"{num_words} words found in the document")
+    letter_count = get_count_letters(text)
+    print(f"Letter count:\n {letter_count}")
+
 def get_count_words(text):
     words = text.split()
     return len(words)
@@ -6,11 +14,15 @@ def get_book_text(path):
     with open(path) as file:
         return file.read()
 
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_count_words(text)
-    print(f"{num_words} words found in the document")
+def get_count_letters(text):
+    lowercase_text = text.lower()
+    letter_count = {}
+    for char in lowercase_text:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+    return letter_count
         
         
 if __name__ == "__main__":
